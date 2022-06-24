d2bf78601b72e2c72634db9d657431977798bfd3
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-06-24 15:47:57 -0500

    set up the server

---
+++ server/server.js
@@ -0,0 +1,25 @@
+// Import express
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
+
+
+// Make an instance of a server
+const app = express();
+const PORT = 5000;
+
+// Serve Static Files
+app.use( express.static( 'server/public' ) );
+
+// Set up middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+// run the server on the port we want
+app.listen( PORT, function(){
+    console.log( 'Server running on PORT', PORT)
+});
